287a8771c3e07f5e61014013ede3cc3cfc4d2bc8
chung140204 <170853492+chung140204@users.noreply.github.com>
2025-04-24 17:30:01 +0700

    Add OrbitControls for mouse view control

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Canvas } from '@react-three/fiber'
 import { VRButton, XR, Controllers, Hands } from '@react-three/xr'
+import { OrbitControls } from '@react-three/drei'
 import { VRScene } from './components/VRScene'
 
 function App() {
   return (
     <>
       <VRButton />
-      <Canvas>
+      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+        <OrbitControls 
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          minDistance={2}
+          maxDistance={10}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2}
+        />
         <XR>
           <Controllers />
           <Hands />
